6971b9ccd033664df4ae2d382008af8bf2e4eee0
ぴか <114275947+1133pikachu@users.noreply.github.com>
2023-12-08 07:27:55 +0900

    Update script.js

--- chapter3/ColorPicker/js/script.js
+++ chapter3/ColorPicker/js/script.js
@@ -2,6 +2,8 @@ const text = document.querySelector('#colorText');
 const color = document.querySelector('#colorPicker');
 
 const colorBg = () => {
+  document.body.style.backgroundColor = color.value;
+  
   text.textContent = `カラーコード:${color.value}`;
 }
 
