dfb61f34262f2a1fe9b8f8c869c72df08fcb0684
ぴか <114275947+1133pikachu@users.noreply.github.com>
2024-04-25 10:41:34 +0900

    Create script.js

---
+++ docs/js/script.js
@@ -0,0 +1,5 @@
+const menu = document.querySelector('#menu');
+
+const content = `<div><img src="https://assets.codepen.io/85188/strawberry.jpg" alt=""></div>`;
+
+menu.insertAdjacentHTML('beforeend', content);
